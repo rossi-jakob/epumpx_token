7dac1d3d032e52038a45fb76aafef9456610705a
spider <jordanspider0101@gmail.com>
2025-05-13 06:20:57 +0000

    thumbnail

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
     title: 'EPumpX',
     description: 'EPumpX is Pumpfun on EPIX chain.',
     images: [{
-      url: '/epix-thumbnail.png',
+      url: 'https://epumpx-token.vercel.app/epix-thumbnail.png',
       width: 1280,
       height: 720,
       alt: 'EPumpX Logo',
@@ -36,7 +36,7 @@ export const metadata: Metadata = {
     card: 'summary_large_image',
     title: 'EPumpXFun',
     description: 'EPumpX is Pumpfun on EPIX chain.',
-    images: ['/epix-thumbnail.png'],
+    images: ['https://epumpx-token.vercel.app/epix-thumbnail.png'],
   },
   // Add these explicit tags to override any potential cached values
   other: {
